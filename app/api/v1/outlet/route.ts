61ea68a5a9498f377a8c124f2698d566bea649a6
Oatu4396 <oatu4396@gmail.com>
2025-04-26 19:04:43 -0400

    Add Reasoning Tokens to Output Tokens Calculations

--- app/api/v1/outlet/route.ts
+++ app/api/v1/outlet/route.ts
@@ -93,7 +93,12 @@ export async function POST(req: Request) {
       lastMessage.usage.completion_tokens
     ) {
       inputTokens = lastMessage.usage.prompt_tokens;
-      outputTokens = lastMessage.usage.completion_tokens;
+      if (lastMessage.usage.completion_tokens_details.reasoning_tokens) {
+        outputTokens = lastMessage.usage.completion_tokens +
+                       lastMessage.usage.completion_tokens_details.reasoning_tokens;
+      } else {
+        outputTokens = lastMessage.usage.completion_tokens;
+      }
     } else {
       outputTokens = encode(lastMessage.content).length;
       const totalTokens = data.body.messages.reduce(
